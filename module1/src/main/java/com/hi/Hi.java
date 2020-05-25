700da624190bd4ce5c1bd122101e202b98f03ae8
Phuoc Le <phuoc.le@unified.com>
2020-05-25 22:48:35 +0700

    Init code

---
+++ module1/src/main/java/com/hi/Hi.java
@@ -0,0 +1,7 @@
+package com.hi;
+
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+    }
+}
---
+++ module2/src/main/java/com/hi2/Hi2.java
@@ -0,0 +1,7 @@
+package com.hi2;
+
+public class Hi2 {
+    public static void main(String[] args) {
+        System.out.println("Hi2!");
+    }
+}
